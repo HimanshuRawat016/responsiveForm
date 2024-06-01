582fcd8912bca04cf9fdeb00d3290ddf627c14f4
HimanshuRawat016 <hrawat016@gmail.com>
2024-06-01 18:28:52 +0530

    defect fix

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
+var userName = "";
+var email = "";
 function showSummary() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
+  userName = document.getElementById("name").value;
+  email = document.getElementById("email").value;
 
-  if (name && email) {
-    alert(`Name:${name} and email:${email}`);
+  if (userName && email) {
     document.getElementById("container").classList.add("active");
-    document.getElementById("form-container").classList.add("notActive");
-    document.getElementById("form-container").classList.remove("active");
-    document.getElementById("form-container").hidden();
+
+    $("#form-container").hide();
   } else {
     alert("Both name and email is mandatory");
   }
+}
+
+var options = [];
+
+$(".option1").click(function () {
+  var id = this.id;
+  $("#" + id).css("background-color", "white");
+  options.push(
+    $("#" + id)
+      .text()
+      .replaceAll("\n", "")
+      .trim()
+  );
+});
+
+$(".option2").click(function () {
+  var id = this.id;
+  $("#" + id).css("background-color", "white");
+  options.push(
+    $("#" + id)
+      .text()
+      .replaceAll("\n", "")
+      .trim()
+  );
+});
+
+$(".option3").click(function () {
+  var id = this.id;
+  $("#" + id).css("background-color", "white");
+  options.push(
+    $("#" + id)
+      .text()
+      .replaceAll("\n", "")
+      .trim()
+  );
+});
+
+function showSummaryDiv() {
+  document.getElementById("container1").classList.add("active");
+  $("#container").hide();
+  var optionSet = new Set(options);
+  $("#nameValue").text(userName);
+  $("#emailValue").text(email);
+  const optionArr = Array.from(optionSet);
+  optionArr.map((element) => {
+    document.getElementById(
+      "topicValue"
+    ).innerHTML += `<div>${element}</div> <br/>`;
+  });
 }
